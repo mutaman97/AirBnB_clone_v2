5ed6798f60307b113720b689224b78e0730b737e
mutaman97 <mutamanelhadi97@gmail.com>
2024-05-17 02:24:29 +0400

    Fix env servers

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.157.161.98', '34.227.90.191', '54.87.206.44']
 
 
 def do_clean(number=0):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.157.161.98', '34.227.90.191', '54.87.206.44']
 
 
 def do_pack():
